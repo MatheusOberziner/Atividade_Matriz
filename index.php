200dfb69469606cd4fe711721a465e265df3365c
Matheus Schmidt Oberziner <matheus.oberziner@unidavi.edu.br>
2022-09-15 11:31:15 -0300

    pronto

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    $arr=array(
+        array(1,"José",6.5),
+        array(2,"Valentina",9),
+        array(3,"Daniel",8.5),
+    );
+
+    print_r($arr[0][0]);
+    echo "<br/>";
+    print_r($arr[0][1]);
+    echo "<br/>";
+    print_r($arr[1][0]);
+    echo "<br/>";
+    print_r($arr[1][1]);
+    echo "<br/>";
+?>
