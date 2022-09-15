e1ba85c89c8563c031a004e8f9cc37d332c8f773
Matheus Oberziner <matheus.oberziner@unidavi.edu.br>
2022-09-15 16:12:55 -0300

    .

--- index.php
+++ index.php
@@ -1,16 +1,28 @@
 <?php
     $arr=array(
-        array(1,"José",6.5),
-        array(2,"Valentina",9),
-        array(3,"Daniel",8.5),
+        array("produto" => "Camisa",
+              "preço" => "R$50,00",
+              "estoque" => 10
+        ),
+        array("produto" => "Calça", 
+              "preço" => "R$70,00",
+              "estoque" => 5
+        ),
+        array("produto" => "Boné", 
+              "preço" => "R$45,00",
+              "estoque" => 8
+        ),
     );
 
-    print_r($arr[0][0]);
+    print_r($arr);
     echo "<br/>";
-    print_r($arr[0][1]);
+    print_r($arr[0]);
     echo "<br/>";
-    print_r($arr[1][0]);
+    print_r($arr[1]);
     echo "<br/>";
-    print_r($arr[1][1]);
+    print_r($arr[2]);
     echo "<br/>";
+
+    $total_reg = count($arr); //variável para número de registros
+    echo "Número de registros {$total_reg}";
 ?>
